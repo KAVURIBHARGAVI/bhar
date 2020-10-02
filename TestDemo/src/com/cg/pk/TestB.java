618f657ef4f09ac351117cb7ada7002cb57d7ee9
bkavuri <bkavuri@LIN86000612.corp.capgemini.com>
2020-10-02 17:25:29 +0530

    HI HELO

---
+++ TestDemo/src/com/cg/pk/TestB.java
@@ -0,0 +1,28 @@
+package com.cg.pk;
+
+import org.junit.Test;
+
+public class TestB {
+
+
+@Test
+public void test1() {
+System.out.println("test1");
+}
+@Test
+public void test2() {
+System.out.println("test2");
+}
+@Test
+public void test3() {
+System.out.println("test3");
+}
+@Test
+public void test4() {
+System.out.println("test4");
+}
+@Test
+public void test5() {
+System.out.println("test5");
+}
+}
